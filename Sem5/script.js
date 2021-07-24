5d0d641832d6379e0f6af77b3a038e7e3befea76
ABD-01 <myac931@gmail.com>
2021-07-24 21:03:12 +0530

    Added JS code, made multiple files, created folders sem wise

---
+++ Sem5/script.js
@@ -0,0 +1,14 @@
+function load() {
+  for (const [slot, subject] of Object.entries(SUBJECTS)) {
+    var anchors = document.getElementsByClassName(slot);
+    for(var i = 0; i < anchors.length; i++) {
+        anchors[i].innerHTML = subject.name;
+        anchors[i].onclick = function() {
+            alert('Slot: '+slot+'\nSubject: '+subject.name+'\nLink: '+subject.link);
+            // location.href=subject.link
+        }
+      }
+    }
+  }
+
+window.onload = load; // onload
