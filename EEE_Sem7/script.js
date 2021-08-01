9aedcedd66521d1de95b131eb96de85272237f49
ABD-01 <myac931@gmail.com>
2021-08-02 00:00:24 +0530

    Updated TT for Sem7

--- EEE_Sem7/script.js
+++ EEE_Sem7/script.js
@@ -4,8 +4,8 @@ function load() {
     for(var i = 0; i < anchors.length; i++) {
         anchors[i].innerHTML = subject.name;
         anchors[i].onclick = function() {
-            alert('Slot: '+slot+'\nSubject: '+subject.name+'\nLink: '+subject.link);
-            // location.href=subject.link
+            // alert('Slot: '+slot+'\nSubject: '+subject.name+'\nLink: '+subject.link);
+            location.href=subject.link
       }
     }
   }
@@ -13,7 +13,7 @@ function load() {
   for (var i = 0; i < 6; i++) {
     for (var j = 0; j < 2; j++) {
       var anchor = document.getElementById('p'+i.toString()+j.toString());
-      anchor.innerHTML = PRACTICALS[i][j].join('<br>')
+      anchor.innerHTML = PRACTICALS[i][j].join('')
     }
   }
 }
--- EEE_Sem7/subjects.js
+++ EEE_Sem7/subjects.js
@@ -12,33 +12,53 @@ const SUBJECTS = {
   },
 
   "slotC": {
-    name: "Introduction to Electrical Vehicles",
+    name: "IEV",
     link: ""
   },
 
   "slotD": {
-    name: "High Voltage Engineering",
+    name: "HVE",
     link: ""
   },
 
   "slotE": {
     name: "HDVC",
-    link: ""
+    link: "https://vnit.webex.com/meet/machaudhari"
   },
 
-  "slotF": {
-    name: "Advanced Power Electronics",
-    link: ""
+  "slotF_tue": {
+    name: "APE",
+    link: "https://vnit.webex.com/vnit/j.php?MTID=mf4c64b01389f79b8af0ca8a267822383"
+  },
+  
+  "slotF_wed": {
+    name: "APE",
+    link: "https://vnit.webex.com/vnit/j.php?MTID=m5e6f897acdfd1b10a19616e3e5a3b177"
+  },
+  
+  "slotF_thur": {
+    name: "APE",
+    link: "https://vnit.webex.com/vnit/j.php?MTID=ma6143fa1d00cf59d0e939185a7559301"
   },
 
   "slotG": {
     name: "Power Quality",
-    link: ""
+    link: "https://meet.google.com/rju-vouo-ncs"
   },
 
   "slotH": {
-    name: "DSP Application to PS",
+    name: "DSP",
+    link: "https://vnit.webex.com/meet/bhooshanar"
+  },
+  
+  "slotPrac": {
+    name: "HVE LAB",
     link: ""
+  },
+
+  "slot": {
+    name: "",
+    link: "#"
   }
 };
 
