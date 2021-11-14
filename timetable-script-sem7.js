c668319f1afb5a4400a5c409f29abbf41adbb98d
lynx1902 <davepushkar02@gmail.com>
2021-11-15 00:17:09 +0530

    timetable & council

---
+++ timetable-script-sem7.js
@@ -0,0 +1,23 @@
+
+function load() {
+    for (const [slot, subject] of Object.entries(SUBJECTS7)) {
+      var anchors = document.getElementsByClassName(slot);
+      for(var i = 0; i < anchors.length; i++) {
+          anchors[i].innerHTML = subject.name;
+          anchors[i].onclick = function() {
+            // alert('Slot: '+slot+'\nSubject: '+subject.name+'\nLink: '+subject.link);
+            if (subject.link != "")
+              window.open(subject.link);
+        }
+      }
+    }
+  
+    // for (var i = 0; i < 6; i++) {
+    //   for (var j = 0; j < 2; j++) {
+    //     var anchor = document.getElementById('p'+i.toString()+j.toString());
+    //     anchor.innerHTML = PRACTICALS7[i][j].join('')
+    //   }
+    // }
+  }
+  
+  window.onload = load; // onload
---
+++ timetable-subjects-sem5.js
@@ -0,0 +1,118 @@
+const SUBJECTS5 = {
+    "5slotA": {
+      name: "---",
+      link: ""
+    },
+  
+    "5slotB": {
+      name: "---",
+      link: ""
+    },
+  
+    "5slotC": {
+      name: "EMC II",
+      link: "https://vnit.webex.com/vnit/k2/j.php?MTID=tf32c53d5c45b4ad99e8a30c02012ceab"
+    },
+  
+    "slotDa5": {
+      name: "EPS sec.A",
+      link: "https://us04web.zoom.us/j/4908242577?pwd=NmVaT0ZmckdCOVJqa2tTcGh6eUdDQT09"
+    },
+    
+     "5slotDb_tue": {
+      name: "EPS sec.B",
+      link: "https://vnit.webex.com/vnit/j.php?MTID=mefbefe3cd5f8c40a4559c6ec66c46be3"
+    },
+    
+     "5slotDb_wed": {
+      name: "EPS sec.B",
+      link: "https://vnit.webex.com/vnit/j.php?MTID=mcda9fca88dd86c4184809992dac1c6e2"
+    },
+    
+     "5slotDb_fri": {
+      name: "EPS sec.B",
+      link: "https://vnit.webex.com/vnit/j.php?MTID=m487be3debc858495245fb760623e0dc6"
+    },
+    
+     "5slotDb_sat": {
+      name: "EPS sec.B",
+      link: "https://vnit.webex.com/vnit/j.php?MTID=mb7b1659275ff5c9084a66c727023339e"
+    },
+  
+    "5slotE": {
+      name: "CSI",
+      link: "https://vnit.webex.com/vnit/j.php?MTID=m2daa28158157529f2b4112d79e0e9aba"
+    },
+  
+    "5slotFa_tue": {
+      name: "EPDS",
+      link: "https://vnit.webex.com/vnit/j.php?MTID=m29d27fedcb3f2da7e0010417a326bbe0"
+    },
+  
+    "5slotFa_wed": {
+      name: "EPDS",
+      link: "https://vnit.webex.com/vnit/j.php?MTID=m076abf70bc686c0a4e546a05c3658db0"
+    },
+  
+    "5slotFa_thu": {
+      name: "EPDS",
+      link: "https://vnit.webex.com/vnit/j.php?MTID=m90eefee0a41e7916f4b1eda32ceac820"
+    },
+  
+    "5slotFb_tue": {
+      name: "OT",
+      link: "https://vnit.webex.com/vnit/j.php?MTID=m77b4f4d72107b22857186444480f44cc"
+    },
+    "5slotFb_wed": {
+      name: "OT",
+      link: "https://vnit.webex.com/vnit/j.php?MTID=m0449013e586155f0fd3209cc4be2c7a2"
+    },
+    "5slotFb_thu": {
+      name: "OT",
+      link: "https://vnit.webex.com/vnit/j.php?MTID=m3ffbe5e61ae1eb70f38b5d2f0aeff1e2"
+    },
+  
+    "5slotG": {
+      name: "EMD",
+      link: "https://vnit.webex.com/vnit/k2/j.php?MTID=tf636d949e2bcf329d668bda4f021cdce"
+    },
+  
+    "5slotH": {
+      name: "IA",
+      link: "https://vnit.webex.com/join/vskale"
+    },
+  
+    "5slot": {
+      name: "",
+      link: ""
+    }
+  };
+  
+  // Practicals
+  
+  const PRACTICALS5 = [
+    [
+      ['EMC-II (B2)', 'CS-I (B4)'], // Mon 4:00pm-4:45pm
+      ['IA (B2)'] // Mon 5:00pm-5:45pm
+    ], 
+    [
+      ['EMC-II (B3)', 'CS-I (B5)'], // Tue 4:00pm-4:45pm
+      ['IA (B3)']  // Tue 5:00pm-5:45pm
+    ], 
+    [
+      ['EMC-II (B4)', 'CS-I (B6)'], // Wed 4:00pm-4:45pm
+      ['IA (B4)']  // Wed 5:00pm-5:45pm
+    ], 
+    [
+      ['EMC-II (B5)', 'CS-I (B1)'], // Thu 4:00pm-4:45pmr
+      ['IA (B5)']  // Thur 5:00pm-5:45pm
+    ], 
+    [
+      ['EMC-II (B6)', 'CS-I (B2)'], // Fri 4:00pm-4:45pm
+      ['IA (B6)']  // Fri 5:00pm-5:45pm
+    ], 
+    [
+      ['EMC-II (B1)', 'CS-I (B3)'], // Sat 4:00pm-4:45pm
+      ['IA (B1)']  // Sat 5:00pm-5:45pm
+    ], 
+  ]
