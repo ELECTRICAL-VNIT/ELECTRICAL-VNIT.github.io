bf128834180046d379c190e3581785ad46c464f1
lynx1902 <davepushkar02@gmail.com>
2022-01-04 20:17:01 +0530

    desktop and mobile tt with minor js issues

--- table.js
+++ table.js
@@ -1,5 +1,19 @@
-if ($(window).width() < 960) {
-     var oldUrl = $('#sem3btn').attr('href');
-     var newUrl = "mobile.html";
-     $('#sem3btn').attr('href',newUrl);
- }
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(
+        " active",
+        ""
+      );
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  // Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
