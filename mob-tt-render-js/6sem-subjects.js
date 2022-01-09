a460e538d12caceb0b075b66510a28df564c76fc
EEEVNIT <87592447+EEEVNIT@users.noreply.github.com>
2022-01-09 22:46:11 +0530

    Update 6sem-subjects.js

--- mob-tt-render-js/6sem-subjects.js
+++ mob-tt-render-js/6sem-subjects.js
@@ -17,7 +17,7 @@ const LEC6 = document.getElementsByClassName("sem6LEC");
 
 for(var i=0; i<LEC6.length; i++) {
 
-   LEC6[i].setAttribute("href","") ;
+   LEC6[i].setAttribute("href","https://vnit.webex.com/meet/srinivassarma.aapp") ;
 
 }
 
@@ -33,7 +33,7 @@ const MicroController = document.getElementsByClassName("sem6MC");
 
 for(var i=0; i<MicroController.length; i++) {
 
-   MicroController[i].setAttribute("href","") ;
+   MicroController[i].setAttribute("href","https://vnit.webex.com/meet/bhooshanar") ;
 
 }
 
@@ -41,7 +41,7 @@ const FACTS = document.getElementsByClassName("sem6FACTS");
 
 for(var i=0; i<FACTS.length; i++) {
 
-   FACTS[i].setAttribute("href","") ;
+   FACTS[i].setAttribute("href","https://meet.google.com/yrd-ugvz-scg") ;
 
 }
 
@@ -49,7 +49,7 @@ const SAP = document.getElementsByClassName("sem6SAP");
 
 for(var i=0; i<SAP.length; i++) {
 
-   SAP[i].setAttribute("href","") ;
+   SAP[i].setAttribute("href","https://vnit.webex.com/join/vskale") ;
 
 }
 
