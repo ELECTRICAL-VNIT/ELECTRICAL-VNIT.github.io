6ac9669efd695177d6aba918f76d95f39b9cdf21
Pushkar Dave <78100512+lynx1902@users.noreply.github.com>
2022-01-31 14:28:47 +0530

    Update 4sem-subjects.js

--- mob-tt-render-js/4sem-subjects.js
+++ mob-tt-render-js/4sem-subjects.js
@@ -56,6 +56,6 @@
 
  }
  
-const LECP = document.getElementByClassname("sem4LECP");
+const LECP = document.getElementsByClassname("sem4LECP");
 
     LECP[0].setAttribute("href","https://vnit.webex.com/vnit/j.php?MTID=m87ea7a6d41a57fead58001d780187373");
